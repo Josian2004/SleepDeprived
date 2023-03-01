cbc2255a3b194f41f52d10031b2fa2d72d2596ab
Josian van Efferen <github@josian.nl>
2023-03-01 10:04:08 +0100

    Fixed main class

--- app/src/main/java/fhict/sm/sleepdeprived/MainActivity.kt
+++ app/src/main/java/fhict/sm/sleepdeprived/MainActivity.kt
@@ -17,27 +17,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SleepDeprivedTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                SleepDeprivedApp()
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Preview(showBackground = true)
+    @Composable
+    fun SleepDeprivedApp() {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SleepDeprivedTheme {
-        Greeting("Android")
     }
 }
