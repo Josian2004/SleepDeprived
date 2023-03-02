56037bba3a3e6904e532181491cc7cf02033b691
Niels <Nielsfeijen76@gmail.com>
2023-03-02 15:13:30 +0100

    progress balls

--- app/src/main/java/fhict/sm/sleepdeprived/HomeScreen.kt
+++ app/src/main/java/fhict/sm/sleepdeprived/HomeScreen.kt
@@ -1,12 +1,11 @@
 package fhict.sm.sleepdeprived
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocalCafe
@@ -18,11 +17,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import fhict.sm.sleepdeprived.ui.theme.aBeeZeeFamily
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 
 
 @Composable
@@ -37,6 +43,7 @@ fun HomeScreen() {
         History()
         Data()
         Caffeine()
+        Spacer(modifier = Modifier.height(10.dp))
     }
 }
 
@@ -73,6 +80,7 @@ fun Header() {
 
 @Composable
 fun History() {
+    val SleptHours = arrayOf(4f, 7f, 6f, 7f, 3f, 8f, 4f, 5f, 6f, 7f,)
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -80,10 +88,113 @@ fun History() {
             .clip(RoundedCornerShape(9.dp))
             .background(MaterialTheme.colors.primary)
             .height(80.dp)
+            .horizontalScroll(enabled = true, state = rememberScrollState(), reverseScrolling = true)
 
     ) {
-
+        progress(SleptHours)
     }
+}
+
+@Composable
+fun progress(SleptHours: Array<Float>) {
+    val Days = arrayOf("21/2", "22/2", "23/2", "24/2", "25/2", "26/2", "27/2", "28/2", "1/3", "2/3")
+    var lastDayDP = 0.dp
+    var i = 0
+    Box(){
+        Row() {
+            Days.forEach { item ->
+                i++
+                if(i == Days.size){
+                    lastDayDP = 15.dp
+                }
+                Column(horizontalAlignment = Alignment.CenterHorizontally,modifier = Modifier.padding(start = 15.dp, top = 10.dp, end = lastDayDP) ) {
+                    CircularProgressbar2(SleptHours[i-1])
+                    Text(
+                        text = item,
+                        color = MaterialTheme.colors.onPrimary,
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+fun CircularProgressbar2(
+    number: Float,
+    numberStyle: TextStyle = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp
+    ),
+    size: Dp = 40.dp,
+    thickness: Dp = 8.dp,
+    animationDuration: Int = 1000,
+    animationDelay: Int = 0,
+    foregroundIndicatorColor: Color = MaterialTheme.colors.onPrimary,
+    backgroundIndicatorColor: Color = MaterialTheme.colors.background,
+    extraSizeForegroundIndicator: Dp = 0.dp
+) {
+
+    // It remembers the number value
+    var numberR by remember {
+        mutableStateOf(-1f)
+    }
+
+    // Number Animation
+    val animateNumber = animateFloatAsState(
+        targetValue = numberR,
+        animationSpec = tween(
+            durationMillis = animationDuration,
+            delayMillis = animationDelay
+        )
+    )
+
+    // This is to start the animation when the activity is opened
+    LaunchedEffect(Unit) {
+        numberR = number
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .size(size = size)
+    ) {
+        Canvas(
+            modifier = Modifier
+                .size(size = size)
+        ) {
+
+            // Background circle
+            drawCircle(
+                color = backgroundIndicatorColor,
+                radius = size.toPx() / 2,
+                style = Stroke(width = thickness.toPx(), cap = StrokeCap.Round)
+            )
+
+            val sleepGoal = 8
+            val sweepAngle = (animateNumber.value / sleepGoal) * 360
+
+            // Foreground circle
+            drawArc(
+                color = foregroundIndicatorColor,
+                startAngle = -90f,
+                sweepAngle = sweepAngle,
+                useCenter = false,
+                style = Stroke((thickness + extraSizeForegroundIndicator).toPx(), cap = StrokeCap.Butt)
+            )
+        }
+
+        // Text that shows number inside the circle
+        Text(
+            text = (animateNumber.value).toInt().toString() + "h",
+            style = numberStyle,
+
+        )
+    }
+
 }
 
 @Composable
@@ -128,7 +239,10 @@ fun Data() {
                     modifier = Modifier.offset(x = 25.dp, y = 20.dp)
                 )
             }
-            Spacer(Modifier.width(20.dp).height(5.dp))
+            Spacer(
+                Modifier
+                    .width(20.dp)
+                    .height(5.dp))
             Box(
                 modifier = Modifier
                     .width(170.dp)
@@ -236,7 +350,10 @@ fun Caffeine() {
                     modifier = Modifier.offset(x = 27.dp, y = 20.dp)
                 )
             }
-            Spacer(Modifier.width(20.dp).height(5.dp))
+            Spacer(
+                Modifier
+                    .width(20.dp)
+                    .height(5.dp))
             Box(
                 modifier = Modifier
                     .width(137.dp)
